83bd5e6ced6c5477d370749903ab2836e33f5d80
MisaelBohorquez27 <67847941+MisaelBohorquez27@users.noreply.github.com>
2024-07-16 21:44:10 -0500

    Agregando

---
+++ swagger.js
@@ -0,0 +1,15 @@
+const swaggerAutogen = require('swagger-autogen')()
+ const doc = {
+   "info": {
+     "title": 'REST API',
+     "description": 'REST API with Express and Firestore'
+   },
+   "host": 'restapi-jja7.onrender.com',
+   "schemes": [
+     "https"
+   ],
+   "basePath": "/api",
+ };
+ const outputFile = './swagger_output.json'
+ const endpointsFiles = ['./routes/*.js']
+ swaggerAutogen(outputFile, endpointsFiles, doc)
